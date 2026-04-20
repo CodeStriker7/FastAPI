b345f1765876f15b33588230fd62672e692fdef9
CodeStriker7 <djozod51@gmail.com>
2026-04-20 12:39:56 +0500

    new func in api/v1/status.py

--- app/api/v1/endpoints/status.py
+++ app/api/v1/endpoints/status.py
@@ -4,4 +4,8 @@ router = APIRouter()
 
 @router.get("/chek")
 def chek_status():
-    return {"status": "Good luck"}
+    return {"status": "Good luck bro"}
+# get /test http://127.0.0.1:8000/api/v1/status/test
+@router.get("/test")
+def again():
+    return {"status": "again try"}
